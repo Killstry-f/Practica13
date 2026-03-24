15fb5e812dc521d15f1b2f9fedeea06cd5d78ecd
Killstry-f <killstry616@gmail.com>
2026-03-24 19:15:34 +0500

    Примеры операторов GROUP\ORDER BY

---
+++ 4AORDERBY.py
@@ -0,0 +1,9 @@
+import sqlite3
+connection = sqlite3.connect('mydatabase.db')
+cursor = connection.cursor ()
+# Выбираем и сортируем пользователей по возрасту по убыванию 
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+results = cursor.fetchall()
+for row in results:
+    print(row)
+connection.close()
