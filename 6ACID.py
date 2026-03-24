84120c864f48621210e98f32e96132fdeaf5ee55
Killstry-f <killstry616@gmail.com>
2026-03-24 19:44:46 +0500

    Транзакции и управление данными

---
+++ 6ACID.py
@@ -0,0 +1,13 @@
+import sqlite3
+# Устанавливаем соединение с базой данных
+with sqlite3.connect('mydatabase.db') as connection:
+    cursor = connection.cursor()
+try:
+    # Начинаем транзакцию автоматически
+    with connection:
+        # Выполняем операции
+        cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user3', 'user3@example.com'))
+        cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user4', 'user4@example.com'))
+except:
+    # Ошибки будут приводить к автоматическому откату транзакции
+    pass
